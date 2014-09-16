ff6c4f9b9dcc39a34767d71707c3a29b73251cca
etpinard <etienne.t.pinard@gmail.com>
2014-09-16 12:03:43 -0400

    make publish w/o 'polynomial-fit' tutorial

--- data_literacy/published/urls.py
+++ data_literacy/published/urls.py
@@ -5,8 +5,6 @@ import learn.views
 
 urlpatterns = patterns(
     '',
-    url("(?P<tutorial>how-to-make-a-polynomial-fit)/$",
-        learn.views.data_literacy_template),
     url("(?P<tutorial>basic-statistics-tutorial)/$",
         learn.views.data_literacy_template)
 )
