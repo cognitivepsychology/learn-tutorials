1d039fa9c51d2305d67e88b0f8dd893a6e1a3b85
etpinard <etienne.t.pinard@gmail.com>
2014-09-30 11:36:16 -0400

    add ability to add id and <a href='#id'> around header

--- _scripts/update_body.py
+++ _scripts/update_body.py
@@ -22,6 +22,23 @@ def wrap(wrappend, wrap_tag, wrap_attrs):
     wrapper.append(contents)
     return
 
+# Strip contents inside tag but keep tag
+def strip_contents(tag):
+    for content in tag.contents:
+        content.extract()
+    return
+
+# Insert tag inside an other
+# source: http://stackoverflow.com/a/21356230/4068492
+def insert(insertend, insert_tag, insert_attrs, insert_content, replace=True):
+    _soup = BeautifulSoup()
+    to_insert = _soup.new_tag(insert_tag, **insert_attrs)
+    to_insert.string = insert_content
+    if replace:
+        strip_contents(insertend)
+    insertend.append(to_insert)
+    return
+
 # -------------------------------------------------------------------------------
 
 # Strip all attributes, <script></script> from body
@@ -48,7 +65,7 @@ def add_lightbox(body):
     for img in Img:
         status.log(NAME, ('Image found! src:', img['src']))
         # If not <a> around <img />, add lightbox !
-        if not img.findParent('a'):
+        if not img.findParent('a'):  # TODO maybe only lightbox <a>
             wrap_attrs = {
                 'href': img['src'],
                 'data-lightbox': os.path.splitext(os.path.basename(img['src']))[0]
@@ -57,6 +74,35 @@ def add_lightbox(body):
             status.log(NAME, ('... wrap with lightbox <a>'))
         else:
             status.log(NAME, ("... <a> found around it, doing nothing"))
+    return body
+
+# Add anchor to headers (for easy link sharing)
+def add_header_anchors(body):
+    H_str = ['h1', 'h2', 'h3', 'h4']
+    for h_str in H_str:
+        H = body.findAll(h_str)
+        insert_tag = 'a'
+        for h in H:
+            text = h.getText(strip=True, separator=u' ')
+            status.log(NAME, ('Header found! text:', text.encode('utf8')))
+            # If <h{} />  is empty, remove it
+            if not text:
+                h.extract()
+                status.log(NAME, ('... is empty, removing it!'))
+                continue
+            # Add id attr to <h{}>
+            _id = text.replace(' ','-').lower()
+            h['id'] = _id
+            status.log(NAME, (
+                "... add id: '{}'"
+            ).format(_id.encode('utf8')))
+            # Add <a href= > around text
+            _href = '#' + _id
+            insert_attrs = {'href': _href}
+            insert(h, insert_tag, insert_attrs, text)
+            status.log(NAME, (
+                "... insert <a href='{}'>"
+            ).format(_href.encode('utf8')))
     return body
 
 # Prettify and remove <body> and </body>
@@ -71,5 +117,6 @@ def prettify(body):
 def update_body(body):
     body = strip(body)
     body = add_lightbox(body)
+    body = add_header_anchors(body)
     body = prettify(body)
     return body
